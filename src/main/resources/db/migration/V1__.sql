4fb430e58489cc2698e8e3fce35d169324279326
Verdox <mail.ysp@web.de>
2026-06-21 23:27:55 +0200

    fix migration

---
+++ src/main/resources/db/migration/V1__.sql
@@ -0,0 +1,10 @@
+CREATE TABLE agent_miner_entity
+(
+    id BINARY (16) NOT NULL,
+    host VARCHAR(255) NULL,
+    port INT          NOT NULL,
+    CONSTRAINT pk_agentminerentity PRIMARY KEY (id)
+);
+
+ALTER TABLE agent_miner_entity
+    ADD CONSTRAINT FK_AGENTMINERENTITY_ON_ID FOREIGN KEY (id) REFERENCES miners (id);
