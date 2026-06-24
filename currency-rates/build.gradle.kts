c90213ede9e9730ae7f470f5dd07f352b05b2ff9
Verdox <mail.ysp@web.de>
2026-06-24 18:53:00 +0200

    fix

--- currency-rates/build.gradle.kts
+++ currency-rates/build.gradle.kts
@@ -40,7 +40,7 @@ tasks.withType<Test> {
 }
 
 tasks.named<BootBuildImage>("bootBuildImage") {
-    environment.put("BP_NATIVE_IMAGE_BUILD_ARGUMENTS", "-march=compatibility --initialize-at-build-time=java.io.ProxyingConsole")
+    environment.put("BP_NATIVE_IMAGE_BUILD_ARGUMENTS", "-march=compatibility")
     environment.put("BP_NATIVE_IMAGE", "true")
     docker {
         publishRegistry {
