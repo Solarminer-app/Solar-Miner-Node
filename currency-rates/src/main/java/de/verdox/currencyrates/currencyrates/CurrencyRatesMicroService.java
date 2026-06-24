f81a91b79fc67a8cabde48e06145b908122324e9
Verdox <mail.ysp@web.de>
2026-06-24 23:10:02 +0200

    add flyway reflection hints

--- currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
+++ currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
@@ -1,12 +1,28 @@
 package de.verdox.currencyrates.currencyrates;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(CurrencyRatesMicroService.FlywayGraalVMHints.class)
 public class CurrencyRatesMicroService {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRatesMicroService.class, args);
     }
 
+    public static class FlywayGraalVMHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(
+                    TypeReference.of("org.flywaydb.core.internal.command.clean.CleanModeConfigurationExtension"),
+                    MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+                    MemberCategory.INVOKE_PUBLIC_METHODS
+            );
+        }
+    }
 }
