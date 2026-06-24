4a311a428538e679fa740523433cfa5222663095
Verdox <mail.ysp@web.de>
2026-06-24 23:30:12 +0200

    Remove flyway hints

--- currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
+++ currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
@@ -9,22 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(CurrencyRatesMicroService.FlywayGraalVMHints.class)
 public class CurrencyRatesMicroService {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRatesMicroService.class, args);
-    }
-
-    public static class FlywayGraalVMHints implements RuntimeHintsRegistrar {
-        @Override
-        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.reflection().registerType(
-                    TypeReference.of("org.flywaydb.core.internal.command.clean.CleanModeConfigurationExtension"),
-                    MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
-                    MemberCategory.INVOKE_PUBLIC_METHODS,
-                    MemberCategory.DECLARED_FIELDS,
-                    MemberCategory.INTROSPECT_DECLARED_METHODS
-            );
-        }
     }
 }
