b2f9008636872dc367a5f08ba07a8549a6112843
Verdox <mail.ysp@web.de>
2026-06-24 23:56:06 +0200

    add hibernate logger to graalvm

--- currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
+++ currency-rates/src/main/java/de/verdox/currencyrates/currencyrates/CurrencyRatesMicroService.java
@@ -9,8 +9,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(CurrencyRatesMicroService.GraalVMHints.class)
 public class CurrencyRatesMicroService {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRatesMicroService.class, args);
+    }
+
+    public static class GraalVMHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(
+                    TypeReference.of("org.hibernate.internal.log.ConnectionInfoLogger"),
+                    MemberCategory.INVOKE_PUBLIC_METHODS
+            );
+
+            hints.reflection().registerType(
+                    TypeReference.of("org.jboss.logging.Logger"),
+                    MemberCategory.INVOKE_PUBLIC_METHODS
+            );
+        }
     }
 }
