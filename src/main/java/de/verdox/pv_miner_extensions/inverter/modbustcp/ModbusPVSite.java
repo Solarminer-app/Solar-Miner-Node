1c3da2d6897ab00a65dc3d841980864e8efa4d34
Verdox <mail.ysp@web.de>
2026-07-01 21:49:57 +0200

    use lombok

--- src/main/java/de/verdox/pv_miner_extensions/inverter/modbustcp/ModbusPVSite.java
+++ src/main/java/de/verdox/pv_miner_extensions/inverter/modbustcp/ModbusPVSite.java
@@ -2,44 +2,15 @@ package de.verdox.pv_miner_extensions.inverter.modbustcp;
 
 import de.verdox.pv_miner.pvsite.PVSiteEntity;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class ModbusPVSite extends PVSiteEntity {
-
     private String ipAddress;
     private int port;
     private int slaveId;
     private String modbusConfigName;
-
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-    public void setIpAddress(String ipAddress) {
-        this.ipAddress = ipAddress;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public int getSlaveId() {
-        return slaveId;
-    }
-
-    public void setSlaveId(int slaveId) {
-        this.slaveId = slaveId;
-    }
-
-    public String getModbusConfigName() {
-        return modbusConfigName;
-    }
-
-    public void setModbusConfigName(String modbusConfigName) {
-        this.modbusConfigName = modbusConfigName;
-    }
 }
--- src/main/java/de/verdox/pv_miner_extensions/inverter/rest/RestPVSite.java
+++ src/main/java/de/verdox/pv_miner_extensions/inverter/rest/RestPVSite.java
@@ -2,48 +2,15 @@ package de.verdox.pv_miner_extensions.inverter.rest;
 
 import de.verdox.pv_miner.pvsite.PVSiteEntity;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class RestPVSite extends PVSiteEntity {
-
     private String hostName;
     private int port;
     private String apiToken;
     private String restPVConfigName;
-
-    public String getHostName() {
-        return hostName;
-    }
-
-    public RestPVSite setHostName(String hostName) {
-        this.hostName = hostName;
-        return this;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public RestPVSite setPort(int port) {
-        this.port = port;
-        return this;
-    }
-
-    public String getApiToken() {
-        return apiToken;
-    }
-
-    public RestPVSite setApiToken(String apiToken) {
-        this.apiToken = apiToken;
-        return this;
-    }
-
-    public String getRestPVConfigName() {
-        return restPVConfigName;
-    }
-
-    public RestPVSite setRestPVConfigName(String restPVConfigName) {
-        this.restPVConfigName = restPVConfigName;
-        return this;
-    }
 }
