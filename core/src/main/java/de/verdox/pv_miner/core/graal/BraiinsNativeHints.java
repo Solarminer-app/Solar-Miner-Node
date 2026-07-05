d2d3790c28e07242888a6a93cfe2f81e64dcbeca
Verdox <mail.ysp@web.de>
2026-07-05 14:46:37 +0200

    fix native image

--- core/src/main/java/de/verdox/pv_miner/core/graal/BraiinsNativeHints.java
+++ core/src/main/java/de/verdox/pv_miner/core/graal/BraiinsNativeHints.java
@@ -9,6 +9,11 @@ public class BraiinsNativeHints implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         hints.resources().registerPattern("graphql/braiins/*.graphql");
+        hints.resources().registerPattern("graphql/braiins/auth/*.graphql");
+        hints.resources().registerPattern("graphql/braiins/miner/*.graphql");
+        hints.resources().registerPattern("graphql/braiins/power/*.graphql");
+        hints.resources().registerPattern("graphql/braiins/bos/*.graphql");
+        hints.resources().registerPattern("graphql/braiins/pool/*.graphql");
         hints.reflection().registerType(BraiinsQuery.class);
     }
 }
